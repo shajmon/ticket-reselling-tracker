5c03209a19425a7ee91ef8b2d9aca47b6536df98
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-04-27 18:52:01 +0200

    add additional properties for the inventory log dto

--- TicketInventoryManager/Models/Entities/InventoryLogDTO.cs
+++ TicketInventoryManager/Models/Entities/InventoryLogDTO.cs
@@ -8,7 +8,9 @@ namespace TicketInventoryManager.Models.Entities
     {
         public int Id { get; set; } 
         public string EventName { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime BuyDate { get; set; }
+        public DateTime? SellDate { get; set; }
         public string VenueName { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
@@ -16,6 +18,14 @@ namespace TicketInventoryManager.Models.Entities
         public int Quantity { get; set; }
         public decimal BuyPerOne { get; set; }
         public decimal? SellPerOne { get; set; }
+        public string BuyPlatform { get; set; }
+        public string AccountEmail { get; set; }
+        public string SellPlatform { get; set; }
+        public Enums.TicketStatus Status { get; set; }
 
+        public decimal? Profit => SellPerOne.HasValue ? (SellPerOne - BuyPerOne) * Quantity : null;
+        public decimal? Roi => SellPerOne.HasValue && BuyPerOne > 0 ?
+            ((SellPerOne - BuyPerOne) / BuyPerOne) * 100 : null;
+        public int? DaysHeld => SellDate.HasValue ? SellDate.Value.Subtract(BuyDate).Days : null;
     }
 }
