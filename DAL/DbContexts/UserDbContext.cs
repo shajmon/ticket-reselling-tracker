1140d13e5e140dd3b26c35012c2410939443cb7f
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-04-27 19:59:42 +0200

    add user db context

---
+++ DAL/DbContexts/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+
+namespace DAL.DbContexts
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("Data Source=users.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 0,
+                    Username = "admin",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("admin"), 
+                    IsAdmin = true
+                }
+            );
+        }
+    }
+}
