8ffc13dea49406bed000e2373b245d0e02218a39
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-04-27 19:12:24 +0200

    add event entity to DAL

--- DAL/Entities/Event.cs
+++ DAL/Entities/Event.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DAL.Enums;
 
 namespace DAL.Entities
 {
-    internal class Event
+    public class Event
     {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public string VenueName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime Date { get; set; }
+        public EventType EventType { get; init; }
     }
 }
