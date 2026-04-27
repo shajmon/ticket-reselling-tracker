c2da5f50ca1562d2d2cab4282479ed58d9aec651
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-04-27 19:33:35 +0200

    add user entity to DAL

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int Id { get; init; }
+        public string Username { get; init; }
+        public string PasswordHash { get; init; }
+        public bool IsAdmin { get; init; }
+
+        public List<InventoryLog> InventoryLogs { get; init; }
+    }
+}
