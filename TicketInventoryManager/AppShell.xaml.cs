e9a247995bb9d1fde31cbbecbdbb2d99f54e61cf
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-04-27 14:18:29 +0200

    create project

---
+++ TicketInventoryManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketInventoryManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
