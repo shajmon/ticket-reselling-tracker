4795f2acfc0771c2338fe82b326b758eaf9210f0
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-05-03 13:47:29 +0200

    add initial viewmodel structure (no impl yet)

---
+++ TicketInventoryManager/ViewModels/LoginViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TicketInventoryManager.Models.Entities;
+using TicketInventoryManager.Services;
+
+namespace TicketInventoryManager.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        private readonly IUserService _userService;
+        [ObservableProperty]
+        public partial string Username { get; set; }
+        [ObservableProperty]
+        public partial string Password { get; set; }
+        [ObservableProperty]
+        public partial string RepeatPassword { get; set; }
+        public bool Registration { get; set; }
+
+        public LoginViewModel(IUserService userService)
+        {
+            _userService = userService;
+            Username = string.Empty;
+            Password = string.Empty;
+            RepeatPassword = string.Empty;
+            Registration = false;
+        }
+
+        [RelayCommand]
+        public async Task TryLogin()
+        {
+            UserDTO? loggedUser = await _userService.LoginAsync(Username, Password);
+
+            if (loggedUser == null)
+            {
+                //invalid username or pass stverec
+            } 
+            else
+            {
+                //reroute na dashboard 
+                //pass userDTO do dalsieho vm
+            }
+        }
+
+        [RelayCommand]
+        public async Task RegisterNewUser()
+        {
+            if (Password == RepeatPassword)
+            {
+                await _userService.RegisterAsync(Username, Password);
+                //reroute na dashboard
+                //pass userDTO do dalsieho vm
+            }
+            else
+            {
+                //passwords nematchuju stverec
+            }
+        }
+
+        [RelayCommand]
+        public void ChangeMode()
+        {
+            Registration = !Registration;
+        }
+    }
+}
