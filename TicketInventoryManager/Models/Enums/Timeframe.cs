a97579d05782abc28f698aabfa6e27e4c7603c3c
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-05-04 23:43:57 +0200

    add timeframe enum

---
+++ TicketInventoryManager/Models/Enums/Timeframe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketInventoryManager.Models.Enums
+{
+    public enum Timeframe
+    {
+        ThisMonth,
+        ThisYear,
+        LastMonth
+    }
+}
