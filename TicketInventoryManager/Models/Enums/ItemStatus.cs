7d554e0fbe4286c84b23e50098125818ceed6e96
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-05-06 14:23:13 +0200

    add item status enum

---
+++ TicketInventoryManager/Models/Enums/ItemStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketInventoryManager.Models.Enums
+{
+    public enum ItemStatus
+    {
+        NotListed, 
+        Listed, 
+        ToDeliver, 
+        Delivered
+    }
+}
