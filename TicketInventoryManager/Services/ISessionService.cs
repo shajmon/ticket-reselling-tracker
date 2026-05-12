5cddb4eb2cb152bb3df50a6b26f068f898ce2a79
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-05-12 10:35:02 +0200

    add session service

---
+++ TicketInventoryManager/Services/ISessionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketInventoryManager.Models.Entities;
+
+namespace TicketInventoryManager.Services
+{
+    internal interface ISessionService
+    {
+        UserDTO? CurrentUser { get; set; }
+    }
+}
---
+++ TicketInventoryManager/Services/SessionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketInventoryManager.Models.Entities;
+
+namespace TicketInventoryManager.Services
+{
+    public class SessionService : ISessionService
+    {
+        public UserDTO? CurrentUser { get; set; }
+    }
+}
