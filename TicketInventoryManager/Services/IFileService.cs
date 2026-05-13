be98c491b103a66a1062978c33bfece5ec0a7e07
Simon Pozgay <xpozgay1@fi.muni.cz>
2026-05-13 14:17:46 +0200

    add file service interface

---
+++ TicketInventoryManager/Services/IFileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TicketInventoryManager.Models.Entities;
+
+namespace TicketInventoryManager.Services
+{
+    public interface IFileService
+    {
+        Task<bool> ExportLogsAsync(ObservableCollection<InventoryLogDTO> logs);
+        Task<ObservableCollection<InventoryLogDTO>?> ImportLogsAsync();
+        Task<bool> ExportEventsAsync(ObservableCollection<EventDTO> events);
+        Task <ObservableCollection<EventDTO>?> ImportEventsAsync();
+    }
+}
